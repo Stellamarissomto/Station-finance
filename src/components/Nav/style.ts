dfb5110907690899c7062b44ec3ddf20519f8dfb
Stellamarissomto <stellamarissomto@gmail.com>
2021-09-27 17:12:01 +0100

    built navbar component for mobile

---
+++ src/components/Nav/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+export const NavStyle = styled.div`
+display: flex;
+
+`
+export const Menu = styled.div`
+flex:15%;
+margin-top: 2rem;
+
+`
+
+export const Logo = styled.div`
+flex: 85%;
+margin-top: 1rem;
+margin-left: 0.5rem;
+
+img {
+    width: 247px;
+    height: 56px;
+}
+
+`
