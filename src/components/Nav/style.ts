43c09fc1698184e2d58ca2fdfef7608d494afe97
Stellamarissomto <stellamarissomto@gmail.com>
2021-09-27 20:15:00 +0100

    desktop responsiveness

--- src/components/Nav/style.ts
+++ src/components/Nav/style.ts
@@ -9,6 +9,10 @@ export const Menu = styled.div`
 flex:15%;
 margin-top: 2rem;
 
+@media (min-width: 1152px){
+    flex:8%;
+}
+
 `
 
 export const Logo = styled.div`
@@ -20,5 +24,38 @@ img {
     width: 247px;
     height: 56px;
 }
+
+@media (min-width: 1152px){
+    flex: 82%;
+    margin-left: 1.2rem;
+}
+
+`
+export const ConnectBtn = styled.div`
+display: none;
+
+@media (min-width: 1152px){
+    display: flex;
+    flex:10%;
+    
+    button {
+        background: #5E6CFA;
+        border: none;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 17px;
+        line-height: 23px;
+        text-align: center;
+        letter-spacing: 0.045em;
+        text-transform: capitalize;
+        color: #FFFFFF;
+        width: 210px;
+        height: 56px;
+        margin-top: 1rem;
+        margin-right: 4rem;
+    }
+    
+  }
 
 `
